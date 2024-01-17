79e252ed328869599b5996602884e2843c001b71
tannpopo <yuailun@pku.edu.cn>
2024-01-17 11:44:10 +0800

    fix: get correct training instances

--- extract_inner_dot_triggered.py
+++ extract_inner_dot_triggered.py
@@ -18,6 +18,7 @@ inner_completion_items_file = f"datasets/strange_identifiers_{forget_gap}_inner_
 with open(completion_items_file, 'r') as f:
     completion_items = json.load(f)
 
+repo_to_files = {}
 for file_path, reqs in tqdm(completion_items.items()):
     # if file_path in valid_file_set:
     #     output_file = valid_file
@@ -27,19 +28,30 @@ for file_path, reqs in tqdm(completion_items.items()):
     repo_name = relative_file_path.split("/")[0]
     relative_file = relative_file_path[len(repo_name)+1:]
     repo_path = os.path.join(repo_root, repo_name)
-    
+    repo_to_files[repo_name] = repo_to_files.get(repo_name, []) + [(file_path, reqs)]
+
+for repo_name, inst in tqdm(repo_to_files.items()):
+    repo_path = os.path.join(repo_root, repo_name)
     lsp = SyncLanguageServer.create(config, _logger, repo_path)
     with lsp.start_server():
-        for req in reqs:
-            trigger_point = req["trigger_point"]
-            definition = lsp.request_definition(relative_file, int(trigger_point[0]), int(trigger_point[1])+1)
-            if len(definition) == 0:
-                req["lib"] = "unknown"
-                continue
-            if definition[0]["absolutePath"].endswith(".class"):
-                req["lib"] = "true"
-            else:
-                req["lib"] = "false"
+        for file_path, reqs in inst:
+            relative_file_path = file_path[len(repo_root):]
+            relative_file = relative_file_path[len(repo_name)+1:]
+            for req in reqs:
+                trigger_point = req["trigger_point"]
+                try:
+                    definition = lsp.request_definition(relative_file, int(trigger_point[0]), int(trigger_point[1])+1)
+                except:
+                    print(f"failed for file {relative_file}, {trigger_point[0]}, {trigger_point[1]}")
+                    continue
+                if len(definition) == 0:
+                    req["lib"] = "unknown"
+                elif definition[0]["absolutePath"].endswith(".class"):
+                    req["lib"] = "true"
+                else:
+                    req["lib"] = "false"
+                with open(inner_completion_items_file+".jsonl", 'a') as f:
+                    f.write(json.dumps(req)+"\n")
 
 with open(inner_completion_items_file, 'w') as f:
     json.dump(completion_items, f, indent=4)
